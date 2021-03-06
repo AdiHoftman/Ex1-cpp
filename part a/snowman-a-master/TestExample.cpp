1ede0daccd766613e4f5ea0c2f719f82cce1ce85
adi <73074680+AdiHoftman@users.noreply.github.com>
2021-03-06 18:58:01 +0200

    final!

--- part a/snowman-a-master/TestExample.cpp
+++ part a/snowman-a-master/TestExample.cpp
@@ -11,12 +11,24 @@
 
 #include "doctest.h"
 #include "snowman.hpp"
-#include <stdexcept>
-#include <iostream>
 using namespace ariel;
 
 #include <string>
+#include <algorithm>
 using namespace std;
+
+/**
+ * Returns the input string without the whitespace characters: space, newline and tab.
+ * Requires std=c++2a.
+ */
+string nospaces(string input) {
+	std:: erase(input, ' ');
+	std::erase(input, '\t');
+	std::erase(input, '\n');
+	std::erase(input, '\r');
+	return input;
+}
+
 
 TEST_CASE("Good snowman code") {
     CHECK(snowman(11114411) == string("_===_\n(.,.)\n( : )\n( : )"));
